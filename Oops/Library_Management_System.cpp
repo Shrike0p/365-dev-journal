f97df944200c7c7575b185e2ce01c38a2f1ba297
Prakhar Singh <prakharsingh2208@gmail.com>
2024-09-22 20:21:31 +0530

    Add library_management_extra

--- Oops/Library_Management_System.cpp
+++ Oops/Library_Management_System.cpp
@@ -38,4 +38,43 @@ class Book{
             cout<<"Book is already borrowed!\n";
         }
     }
-}
+
+    void returnBook(){
+        if(!isAvailable){
+            isAvailable=true;
+        }
+    }
+
+};
+
+class User{
+    private:
+        string name;
+        vector<Book*>borrowedBooks;
+        static const int borrowLimit=3;
+    public:
+        User(string n) : name(n){}
+
+        string getName() const {return name;}
+
+        void borrowBook(Book &book){
+            if(borrowedBooks.size()>=borrowLimit){
+                cout <<"You have reached the borrowing limit of"<<borrowLimit<<"books\n";
+                return  ;
+            }
+
+            if(book.available()){
+                book.borrowBook();
+                borrowedBooks.push_back(&book);
+                cout<<"You borrowed: ";
+            }else{
+                cout<<"Book is not available.\n";
+            }
+        } 
+
+        void returnBook(Book &book){
+            for (size_t i=0;i<borrowedBooks.size();++i){
+                if(borrowedBooks[i]->getIsbn()==)
+            }
+        }
+};
